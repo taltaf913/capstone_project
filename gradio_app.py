039338dc795731e59384186776b9b34d5d91f926
Giri Prathivadi <128894310+giriprathivadi@users.noreply.github.com>
2023-11-25 12:06:36 +0530

    ChatGPT integration is done
    A sample ChatGPT integration is done. As of now, when you click on Predict button, it show the possible diseases for a mango plant

--- gradio_app.py
+++ gradio_app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+import openai
 
 
 myControls = {
@@ -8,13 +9,26 @@ myControls = {
 }
 def predict(imageToProcess):
     #myControls["ResultContro"].value = "No Disease"
+    openai.api_key = "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"
+    message = "Mango Plant Diseases" 
+    if message: 
+        messages = []
+        messages.append( 
+            {"role": "user", "content": message}, 
+        ) 
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=messages 
+        ) 
+      
+    reply = chat.choices[0].message.content 
+    #messages.append({"role": "assistant", "content": reply})
 
-    return ["No Disease", "Nothing is required"]
+    return ["No Disease", reply]
 
 def submitFeedback(a,b):
     return ["User input submitted successfully"]
 
-with gr.Blocks() as app :
+with gr.Blocks(allow_flagging="manual") as app :
 
     gr.Markdown(
     """
